b554190b85500248d5190076bfe40d031a4e2a9c
Robin Kataria <robin8kataria@gmail.com>
2020-05-05 23:16:41 +0530

    Updates

---
+++ precache-manifest.8acf5a1bbae2c4355c49ae347d005444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea25a2425e76899548010c7a90dfc57",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "0814678bf11f6db1baf7",
+    "url": "/Monsters-Rolodex/static/css/main.6f9086c2.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/Monsters-Rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0814678bf11f6db1baf7",
+    "url": "/Monsters-Rolodex/static/js/main.4d939502.chunk.js"
+  },
+  {
+    "revision": "214264927cff15dd8c24",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.78d04239.js"
+  }
+]);
